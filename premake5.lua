0a6d26ef1fd6a1ce46e3ac5bf291d9af7d5b8615
JohnMarcus <57123223+mynahcode@users.noreply.github.com>
2023-05-06 02:07:10 -0700

    Errors/typos in GLFW's premake5.lua file fixed.

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 
-	targetdir ("bin/" ... outputdir .. "/%{prj.name}")
-	objdir ("obj/" ... outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
@@ -16,12 +16,12 @@ project "GLFW"
 		"src/internal.h",
 		"src/monitor.c",
 		"src/null_init.c",
-		"src/null_joystick.c"
+		"src/null_joystick.c",
 		"src/null_monitor.c",
 		"src/null_window.c",
+		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c"
-		"src/platform.c",
 	}
 	
 	filter "system:windows"
@@ -32,7 +32,7 @@ project "GLFW"
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
-			"src/win32_module.c"
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
